2e2a286873a00b5b1d7a2d3a1cde17f9e28a8bd4
Fatih <kresnofatihimani.private@gmail.com>
2021-01-18 14:21:15 +0700

    #11-end async-await

--- asyapi.js
+++ asyapi.js
@@ -1,10 +1,33 @@
 // fetch api
 
-fetch('todos/todos2.json').then((response)=>{
-    console.log(response);//resolved
-    return response.json();
-}).then(data=>{
-    console.log(data);
-}).catch(err=>{
-    console.log(err);//reject
-});
+// fetch('todos/todos2.json').then((response)=>{
+//     console.log(response);//resolved
+//     return response.json();
+// }).then(data=>{
+//     console.log(data);
+// }).catch(err=>{
+//     console.log(err);//reject
+// });
+
+console.log(1);
+console.log(2);
+console.log(3);
+
+// async-await function
+const getTodos = async () => {
+    const response = await fetch('todos/todos4.json');
+    if(response.status!==200){
+        throw new Error('Cannot fetch data');
+    }
+    const data = await response.json();
+    // console.log(data);
+    return data;
+};
+getTodos()
+    .then(data=>{
+        console.log(data);
+    })
+    .catch(err=>{
+        console.log(err.message);
+    });
+console.log(4);
