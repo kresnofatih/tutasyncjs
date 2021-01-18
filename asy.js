31ed78e123d4a00a1c90aad0a46fa1a57c22fa1c
Fatih <kresnofatihimani.private@gmail.com>
2021-01-18 13:40:14 +0700

    lesson 7-end added promise in function

--- asy.js
+++ asy.js
@@ -1,43 +1,29 @@
 // What is Async JavaScript
 
 // Making a request object
-const getTodos = (resource, callback) => {
-    const request = new XMLHttpRequest();
-    request.addEventListener('readystatechange', ()=>{
-        // console.log(request, request.readyState);
-        // when ready status = 4 (data completed download or error)
-        if (request.readyState === 4 && request.status === 200){
-            const data = JSON.parse(request.responseText)
-            // console.log(request, request.responseText);
-            callback(undefined, data);
-        } 
-        else if(request.readyState===4){
-            // console.log('cant fetch data');
-            callback('cant fetch data', undefined);
-        }
+const getTodos = (resource) => {
+    return new Promise((resolve, reject)=>{
+        const request = new XMLHttpRequest();
+        request.addEventListener('readystatechange', ()=>{
+            // console.log(request, request.readyState);
+            // when ready status = 4 (data completed download or error)
+            if (request.readyState === 4 && request.status === 200){
+                const data = JSON.parse(request.responseText)
+                resolve(data);
+            } 
+            else if(request.readyState===4){
+                reject('error getting resource');
+            }
+        });
+        // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+        // request.open('GET', 'todos.json');
+        request.open('GET', resource);
+        request.send();
     });
-    // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-    // request.open('GET', 'todos.json');
-    request.open('GET', resource);
-    request.send();
 };
-console.log(1);
-console.log(2);
-getTodos('todos/todos.json', (err, data)=>{
-    // console.log('callback fired');
-    console.log(data);
-    getTodos('todos/todos2.json', (err, data)=>{
-        console.log(data);
-        getTodos('todos/todos3.json', (err, data)=>{
-            console.log(data);
-        })
-    })
-    // console.log(err, data);
-    // if(err){
-    //     console.log(err);
-    // } else {
-    //     console.log(data);
-    // }
-});
-console.log(3);
-console.log(4);
+
+getTodos('todos/todos.json').then((data)=>{
+    console.log(data);//resolved
+}).catch(err=>{
+    console.log(err);//rejected
+})
